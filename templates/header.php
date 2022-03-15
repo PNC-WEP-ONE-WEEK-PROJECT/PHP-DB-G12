8bcb4f6105fa8ce325d217dd3bef262e2d1d6147
mengyer34 <mengyi.yoeng34@gmail.com>
2022-03-15 16:14:01 +0700

    header

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facebook 2.0</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <!-- Your style here -->
 </head>
+<body>
+<div class="min-h-full">
+  <nav class="bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
+    <div class="flex items-stretch p-3">
+        <div class="self-center flex items-center ml-5 space-x-4 w-1/3">
+            <img src="../images/logo.png" alt="logo" width="50px">
+            <label class="relative block">
+                <span class="sr-only">Search</span>
+                <span class="absolute inset-y-0 left-0 flex items-center">
+                    <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><img src="../images/icon/search.svg" alt=""></svg>
+                </span>
+                <input class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 ml-3  py-2 pl-9 pr-7 shadow-sm focus:outline-none rounded-full focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search Facebook" type="text" name="search"/>
+            </label>
+            <!-- <input type="text" name="search-friend" id="search" class="focus:ring-indigo-500 focus:border-indigo-500 block w-60 pl-9 pr-7 sm:text-sm border-gray-300 rounded-full p-2 border-2" placeholder="Search Facebook"> -->
+        </div>
+        <div class="self-center flex items-center justify-center space-x-20 w-1/3">
+            <a href="#" class=""><img src="../images/icon/home.svg" alt="home"></a>
+            <a href="#"><img src="../images/icon/friends.svg" alt="friend"></a>
+        </div>
+        <div class="self-center flex w-1/3 justify-end mr-5">
+            <a href="#"><img class="rounded-full hover:border-4 border-blue-400 text-cyan-500" src="../images/rady.jpg" alt="home" width="50px"></a>
+        </div>
+    </div>
+  </nav>
+</div>
+
+
+
 <body>
     
